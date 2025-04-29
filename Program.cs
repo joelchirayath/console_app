f94c14b5ff6dd117a8d2ecf8c88b301d8b786aa9
Joel Chirayath <joeldavidraphel@gmail.com>
2025-04-29 18:41:25 +0300

    Step 1: Initial JSON deserialization example

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+class Program
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        string filePath = "user.json";
+        string jsonResponse = File.ReadAllText(filePath);
+        User user = JsonConvert.DeserializeObject<User>(jsonResponse);
+
+        Console.WriteLine($"Name: {user.Name}");
+        Console.WriteLine($"Age: {user.Age}");
+        Console.WriteLine($"City: {user.City}");
+    }
+}
+ 
