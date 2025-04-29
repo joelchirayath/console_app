9bddea8c78e8128ca50c7d84724c50d12289556b
Joel Chirayath <joeldavidraphel@gmail.com>
2025-04-29 20:24:33 +0300

    Step 2: Added XML deserialization with XmlSerializer

--- Program.cs
+++ Program.cs
@@ -1,25 +1,73 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 
-class Program
+namespace ConsoleApp
 {
-    public class User
+    // JSON User class
+    public class JsonUser
     {
         public string Name { get; set; }
         public int Age { get; set; }
         public string City { get; set; }
     }
 
-    static void Main(string[] args)
+    // XML User class MUST be public!
+    [XmlRoot("User")]
+    public class XmlUser
     {
-        string filePath = "user.json";
-        string jsonResponse = File.ReadAllText(filePath);
-        User user = JsonConvert.DeserializeObject<User>(jsonResponse);
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
 
-        Console.WriteLine($"Name: {user.Name}");
-        Console.WriteLine($"Age: {user.Age}");
-        Console.WriteLine($"City: {user.City}");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadJson();
+            ReadXml();
+        }
+
+        static void ReadJson()
+        {
+            Console.WriteLine("--- JSON User Data ---");
+
+            string filePath = "user.json";
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("JSON file not found!");
+                return;
+            }
+
+            string json = File.ReadAllText(filePath);
+            JsonUser user = JsonConvert.DeserializeObject<JsonUser>(json);
+
+            Console.WriteLine($"Name: {user.Name}");
+            Console.WriteLine($"Age: {user.Age}");
+            Console.WriteLine($"City: {user.City}");
+        }
+
+        static void ReadXml()
+        {
+            Console.WriteLine("\n--- XML User Data ---");
+
+            string filePath = "example.xml";
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("XML file not found!");
+                return;
+            }
+
+            XmlSerializer serializer = new XmlSerializer(typeof(XmlUser));
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                XmlUser user = (XmlUser)serializer.Deserialize(reader);
+                Console.WriteLine($"Name: {user.Name}");
+                Console.WriteLine($"Age: {user.Age}");
+                Console.WriteLine($"City: {user.City}");
+            }
+        }
     }
 }
- 
